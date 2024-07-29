8054400df64cbdb057dee21d8eaa3ab5169312cf
includeSimon <simon.pirlog@yahoo.com>
2024-07-29 12:01:12 +0300

    added CI/CD for FrontEnd

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,27 @@
 import json
+import boto3
 
-# import requests
-
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('cloudresume-test')
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    response = table.get_item(Key={'id': '1'})
+    if 'Item' in response:
+        views = response['Item']['views']
+    else:
+        views = 0
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    views += 1
+    table.put_item(Item={'id': '1', 'views': views})
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "https://simondevops.online",  # Configure CORS
+        },
         "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
+            "views": views,
         }),
-    }
+    } 
+    
